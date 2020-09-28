f7d2c6ef3270891af5713e8af7cc6b8d7d21e859
kekusmacca <71947121+kekusmacca@users.noreply.github.com>
2020-09-28 14:37:36 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,59 +1,76 @@
 <?php
 //echo ;
-$hello = "hello world.";
-
-//echo 'Percentage: ' . htmlspecialchars($_GET["percent"]);
-
-// Define Now
-$now = time(); // or your date as well
-
-// Define $start_date
-$start_date = strtotime("2020-01-01");
-
-// Define $end_date
-$end_date = strtotime("2020-12-31");
-
-// Calculate total duration
-$date_diff = $end_date - $start_date;
-$total_days = round($date_diff/ (60 * 60 * 24));
-echo "<br />";
-echo "Total Days(duration): " . $total_days;
-
-// Number of days passed
-$date_diff = $now - $start_date;
-$days_passed = round($date_diff / (60 * 60 * 24));
-echo "<br />";
-echo "Number of days passed: " . $days_passed ;
-
-// Calculate percentage completed from todays date
-// P = (Y/X)*100;
-//watch out for divide by 0
-$percent = round(($days_passed/$total_days)*100);
-
-// Calclulate days left
-// TODO: Implement days left
-
-// Display Progress bar
-echo "<br />";
-echo "Percent: " . $percent ;
-$progressBar = "<div class=\"dpb-blue\" style=\"height:24px;width:${percent}%\">${percent}%</div>";
+include_once("include/dgpb.php");
 
  ?>
  <html>
  <title>Progress Bar Test Page</title>
  <meta name="viewport" content="width=device-width, initial-scale=1">
- <link rel="stylesheet" href="css/dpb.css">
+ <link rel="stylesheet" href="css/dgpb.css">
  <body>
- <div class="dpb-container">
+ <div class="dgpb-container">
 
- <h2>Progress Bar</h2>
- <p>The dpb-container class can be used for a progress bar.</p>
- <p>The CSS width property can be used to set the height and width.</p>
+ <h2>Days Gone Progress Bar</h2>
+ <p>Test page.</p>
+ <?php
+ try { // every thing fine
+	echo "Normal progression<br />";
+    $pb = new daysGoneProgressBar("2020-01-01", "2020-12-31"); //YYYY-MM-DD
+	//$pb->showInfo();
+	$pb->displayBar();
+} catch (Exception $e) {
+    echo $e->getMessage();
+}
 
- <div class="dpb-border">
-   <?php echo $progressBar ?>
- </div>
+try { // deadline close
+	echo "Deadline is close.<br />";
+    $pb = new daysGoneProgressBar("2020-03-01", "2020-10-05");	
+	$pb->displayBar();
+} catch (Exception $e) {
+    echo $e->getMessage();
+}
 
+try { // today is the day
+	echo "Today is the last day.  Deadlline.<br />";
+    $pb = new daysGoneProgressBar("2020-03-01", "2020-09-28");	
+	$pb->displayBar();
+} catch (Exception $e) {
+    echo $e->getMessage();
+}
+
+try { // today is passed deadline
+    $pb = new daysGoneProgressBar("2020-03-01", "2020-09-26");
+	echo "Deadline passed<br />";
+	$pb->displayBar();
+} catch (Exception $e) {
+    echo $e->getMessage();
+}
+
+try { // first day not here yet
+    $pb = new daysGoneProgressBar("2020-10-01", "2020-10-26");
+	echo "First day not here yet<br />";
+	$pb->displayBar();
+} catch (Exception $e) {
+    echo $e->getMessage();
+}
+
+try { // dates reversed
+	echo "Dates reversed<br />";
+    $pb = new daysGoneProgressBar("2021-01-01", "2020-12-31"); //YYYY-MM-DD
+	$pb->displayBar();
+} catch (Exception $e) {
+    echo $e->getMessage();
+}
+
+try { // bad date data
+	echo "Bad dates<br />";
+    $pb = new daysGoneProgressBar("20d20-01-01", "2020-12-31");
+	$pb->displayBar();
+} catch (Exception $e) {
+    echo $e->getMessage();
+}
+
+ ?>
  </div>
  </body>
  </html>
