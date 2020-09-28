6f45f442532bdadd02eba4e62581e8e18af13fc8
kekusmacca <71947121+kekusmacca@users.noreply.github.com>
2020-09-27 19:04:10 -0500

    Implemented most functionality

--- index.php
+++ index.php
@@ -1,16 +1,43 @@
 <?php
 //echo ;
 $hello = "hello world.";
-$start_date = "";
-$end_date = "";
-echo 'Percentage: ' . htmlspecialchars($_GET["percent"]) . '!';
-$percentDone = htmlspecialchars($_GET["percent"]);
-$progressBar = "<div class=\"dpb-blue\" style=\"height:24px;width:${percentDone}%\"></div>";
+
+//echo 'Percentage: ' . htmlspecialchars($_GET["percent"]);
+
+// Define Now
+$now = time(); // or your date as well
 
 // Define $start_date
+$start_date = strtotime("2020-01-01");
+
 // Define $end_date
-// Calculate number of days
+$end_date = strtotime("2020-12-31");
+
+// Calculate total duration
+$date_diff = $end_date - $start_date;
+$total_days = round($date_diff/ (60 * 60 * 24));
+echo "<br />";
+echo "Total Days(duration): " . $total_days;
+
+// Number of days passed
+$date_diff = $now - $start_date;
+$days_passed = round($date_diff / (60 * 60 * 24));
+echo "<br />";
+echo "Number of days passed: " . $days_passed ;
+
 // Calculate percentage completed from todays date
+// P = (Y/X)*100;
+//watch out for divide by 0
+$percent = round(($days_passed/$total_days)*100);
+
+// Calclulate days left
+// TODO: Implement days left
+
+// Display Progress bar
+echo "<br />";
+echo "Percent: " . $percent ;
+$progressBar = "<div class=\"dpb-blue\" style=\"height:24px;width:${percent}%\">${percent}%</div>";
+
  ?>
  <html>
  <title>dpb.CSS</title>
